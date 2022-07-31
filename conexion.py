28b66293cf3634c656cfc9ad7ff7e64739740a3a
Javier Andrade <javieregonzaleza@gmail.com>
2022-07-31 14:31:38 -0500

    Probando app existente

--- conexion.py
+++ conexion.py
@@ -3,14 +3,17 @@ import mysql.connector  #pip install mysql-connector-python
 class Registro_datos():
 
     def __init__(self):
-        self.conexion = mysql.connector.connect( host='localhost', database ='crepas', user = 'root', password ='Nanda')
+        self.conexion = mysql.connector.connect( host='localhost',
+                                            database ='base_datos', 
+                                            user = 'root',
+                                            password ='Nanda')
 
 
 
-    def inserta_producto(self,Id, Costo, Unidades, Precio, Utilidad, Fecha):
+    def inserta_producto(self,codigo, nombre, modelo, precio, cantidad):
         cur = self.conexion.cursor()
-        sql='''INSERT INTO ventas (Id, Costo, Unidades, Precio, Utilidad, Fecha) 
-        VALUES('{}', '{}','{}', '{}','{}')'''.format(Id, Costo, Unidades, Precio, Utilidad, Fecha)
+        sql='''INSERT INTO productos (CODIGO, NOMBRE, MODELO, PRECIO, CANTIDAD) 
+        VALUES('{}', '{}','{}', '{}','{}')'''.format(codigo, nombre, modelo, precio, cantidad)
         cur.execute(sql)
         self.conexion.commit()    
         cur.close()
@@ -18,14 +21,14 @@ class Registro_datos():
 
     def mostrar_productos(self):
         cursor = self.conexion.cursor()
-        sql = "SELECT * FROM crepas " 
+        sql = "SELECT * FROM productos " 
         cursor.execute(sql)
         registro = cursor.fetchall()
         return registro
 
     def busca_producto(self, nombre_producto):
         cur = self.conexion.cursor()
-        sql = "SELECT * FROM crepas WHERE NOMBRE = {}".format(nombre_producto)
+        sql = "SELECT * FROM productos WHERE NOMBRE = {}".format(nombre_producto)
         cur.execute(sql)
         nombreX = cur.fetchall()
         cur.close()     
@@ -33,8 +36,7 @@ class Registro_datos():
 
     def elimina_productos(self,nombre):
         cur = self.conexion.cursor()
-        sql='''DELETE FROM crepas WHERE NOMBRE = {}'''.format(nombre)
+        sql='''DELETE FROM productos WHERE NOMBRE = {}'''.format(nombre)
         cur.execute(sql)
         self.conexion.commit()    
         cur.close()
-  
